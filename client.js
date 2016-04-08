9572cab102935a1977879b69c4c17d0b4b014f6c
Phil Rajchgot <tophil@outlook.com>
2016-04-07 23:35:49 -0400

    added click event handling for buttons, calculator is now usable

--- client.js
+++ client.js
@@ -1,6 +1,46 @@
 var $ = document.querySelector.bind(document);
 var $all = document.querySelectorAll.bind(document);
+var displayingResult = false;
 
-document.onload = function () {
 
+window.onload = function () {
+  var buttons = $all('button');
+
+  for (var i = 0; i < buttons.length; i++) {
+    // equals button needs special behavior
+    if (buttons[i].value === '=') continue;
+
+    buttons[i].addEventListener('click', updateDisplay);
+  }
+
+  $('#equals').addEventListener('click', handleEquals);
+}
+
+
+
+function updateDisplay (e) {
+  if (displayingResult) {
+    // if a result is on the display, replace the text
+    $('.display').textContent = e.target.value;
+    displayingResult = false;
+  }
+  else {
+    // otherwise, append to the text
+    $('.display').textContent += e.target.value;
+  }
+}
+
+
+
+function handleEquals (e) {
+  var expression = $('.display').textContent;
+
+  try {
+    $('.display').textContent = startParsing(expression);
+    displayingResult = true;
+  }
+  catch (e) {
+    $('.display').textContent = e.toString();
+    displayingResult = true;
+  }
 }
